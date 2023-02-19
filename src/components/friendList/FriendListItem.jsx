34efd75142ddec254b0a01656a834ccd3a333d31
Artem Riabchych <artemskyart@gmail.com>
2023-02-19 23:48:13 +0200

    fix

--- src/components/friendList/FriendListItem.jsx
+++ src/components/friendList/FriendListItem.jsx
@@ -4,11 +4,10 @@ import css from './FriendListItem.module.css';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      <span className={css.status} style={{ background: (isOnline? "green" : "red") }}>
-        {isOnline}
-      </span>
+      
       <img className={css.avatar} src={avatar} alt={name} />
       <span className={css.name}>{name}</span>
+      <span className={css.status} style={{ background: (isOnline? "green" : "red") }}></span>
     </li>
   );
 };
